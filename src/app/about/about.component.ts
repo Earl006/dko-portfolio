698606bb1719a47666c0cae1a37dd66e36334320
Earl006 <earljoe06@gmail.com>
2024-11-09 02:20:17 +0300

    [Added]: portfolio website + docs

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+// about.component.ts
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  templateUrl: './about.component.html',
+})
+export class AboutComponent {}
